51706d6085c340e875a0cbae080f65701c4bbc86
pedrosapiens <pedro@sapienslearning.com>
2021-11-07 13:04:48 +0100

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'p01estructura';
+  message: string = 'Hola Universo!'
 }
